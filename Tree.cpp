0bc8bd7777e60525dbf31903bee74540bda1907b
DZand <48325754+DZand@users.noreply.github.com>
2019-03-24 14:29:13 +0200

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -39,7 +39,7 @@ Tree::Tree(Node n)
     }
     bool Tree:: contains(int a)
     {
-        node *current = root;	
+        Node *current = root;	
         while(current->data != data)
         {
 	
